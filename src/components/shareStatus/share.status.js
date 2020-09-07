90777e9583ee85fc398b5773d6b7bde11858beab
przemskydev <przemekgithub@o2.pl>
2020-09-07 22:51:15 +0200

    added button to share status

--- src/components/shareStatus/share.status.js
+++ src/components/shareStatus/share.status.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 
-const shareStatus = () => <h1>Say hello here!</h1>;
+const ShareStatus = () => (
+  <>
+    <Card style={{ width: '60rem' }}>
+      <Card.Body>
+        <Card.Title>
+          <Row>
+            <Col xs={1}>M</Col>
+            <Col xs={9}>Mark Otto</Col>
+            <Col xs={2}>Profile</Col>
+          </Row>
+        </Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of the card
+          content.
+        </Card.Text>
+        <div className="d-flex justify-content-between">
+          <Button variant="secondary">FILE</Button>
+          <Button variant="secondary">SHARE</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  </>
+);
 
-export default shareStatus;
+export default ShareStatus;
--- src/views/Main.view.js
+++ src/views/Main.view.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ShareStatus from '../components/shareStatus/share.status';
 
 const MainView = () => {
   return (
     <>
       <h1>Hello there!</h1>
+      <ShareStatus />
     </>
   );
 };
