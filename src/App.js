021c335da383f70298fe228079ccb8250d70b51b
przemskydev <przemekgithub@o2.pl>
2020-09-07 15:16:17 +0200

    initial basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import MainView from './views/Main.view'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { mainView, login, register } from './routes/index'
+import MainView from './views/Main.view';
+import Login from './views/Login.view';
+import Register from './views/Register.view';
+
 import './App.css';
 
 function App() {
   return (
-    <MainView />
+    <Router>
+      <Route exact path="/" component={MainView} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
---
+++ src/views/Register.view.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Register = () => <h1>Register panel</h1>;
+
+export default Register;
